b9bff8bb7c7556cbbd5987a456c921a228ebdd05
jchemell <juanchemell@gmail.com>
2020-03-26 16:08:05 -0300

    Finalizado el ejercicio!

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ const port = 3000;
 const server = express();
 server.listen(port, console.log(`Server UP on port: ${port}`));
 
-//! MIDDLEWARES
-server.use(bodyParser.json());
-
-//! ROUTES
-server.get('/autores', (req, res) => {
-	console.log(req.method, req.path, 'Autores');
-	res.status(200).send(autores);
-});
-
 //! DATABASE
 var idLibros = 3;
 const autores = [
@@ -61,53 +52,81 @@ const autores = [
 	}
 ];
 
-server.post('/autores', (req, res) => {
+//! MIDDLEWARES
+server.use(bodyParser.json());
+
+function existeAutor(req, res, next) {
+	let { nombre, apellido } = req.body;
+	let existe = autores.find(autor => autor.nombre === nombre && autor.apellido === apellido);
+	if (existe) return res.sendStatus(409);
+	next();
+}
+
+function existeAutorID(req, res, next) {
+	let { id } = req.params;
+	let existe = autores.find(autor => autor.id === parseInt(id));
+	if (!existe) return res.sendStatus(404);
+	next();
+}
+
+function existeLibroID(req, res, next) {
+	let { id, idLibro } = req.params;
+	let { libros } = autores.find(autor => autor.id === parseInt(id));
+	let libro = libros.find(libro => libro.id === parseInt(idLibro));
+	if (!libro) return res.sendStatus(404);
+	next();
+}
+
+//! ROUTES /AUTORES
+server.get('/autores', (req, res) => {
+	res.status(200).send(autores);
+});
+
+server.post('/autores', existeAutor, (req, res) => {
 	let autor = req.body;
 	autor.id = autores.length + 1;
 	autor.libros = [];
 	autores.push(autor);
-	console.log(autores);
 	res.status(201).send(autor);
 });
 
-server.get('/autores/:id', (req, res) => {
-	let autor = autores.find(autor => autor.id === parseInt(req.params.id));
+server.get('/autores/:id', existeAutorID, (req, res) => {
+	let { id } = req.params;
+	let autor = autores.find(autor => autor.id === parseInt(id));
 	res.status(200).send(autor);
 });
 
-server.delete('/autores/:id', (req, res) => {
-	let index = autores.findIndex(autor => autor.id === parseInt(req.params.id));
+server.delete('/autores/:id', existeAutorID, (req, res) => {
+	let { id } = req.params;
+	let index = autores.findIndex(autor => autor.id === parseInt(id));
+
 	autores.splice(index, 1);
-	console.log('Los autores son:', autores);
 	res.sendStatus(204);
 });
 
-server.put('/autores/:id', (req, res) => {
+server.put('/autores/:id', existeAutorID, (req, res) => {
 	let { id } = req.params;
 	let { nombre, apellido, fechaDeNacimiento } = req.body;
 
 	let nuevoAutor = autores.find(autor => autor.id === parseInt(id));
-	console.log('AUTOR:', nuevoAutor);
-	if (!nuevoAutor) return res.status(404).send('No existe el autor');
 	let index = autores.findIndex(autor => autor.id === parseInt(id));
-	console.log('INDEX:', index)
 
 	nuevoAutor.id = parseInt(id);
 	nuevoAutor.nombre = nombre;
 	nuevoAutor.apellido = apellido;
 	nuevoAutor.fechaDeNacimiento = fechaDeNacimiento;
-	console.log('NUEVO AUTOR', nuevoAutor)
 
 	autores.splice(index, 1, nuevoAutor);
-	res.sendStatus(204);
+	res.status(200).send(autores[index]);
 });
 
-server.get('/autores/:id/libros', (req, res) => {
+//! ROUTES /LIBROS
+server.get('/autores/:id/libros', existeAutorID, (req, res) => {
 	let { libros } = autores.find(autor => autor.id === parseInt(req.params.id));
 	res.send(libros);
 });
 
-server.post('/autores/:id/libros', (req, res) => {
+server.post('/autores/:id/libros', existeAutorID, (req, res) => {
 	let index = autores.findIndex(autor => autor.id === parseInt(req.params.id));
 
 	let libro = req.body;
@@ -115,5 +134,38 @@ server.post('/autores/:id/libros', (req, res) => {
 
 	autores[index].libros.push(libro);
 
+	res.status(201).send(libro);
+});
+
+server.get('/autores/:id/libros/:idLibro', existeAutorID, existeLibroID, (req, res) => {
+	let { id, idLibro } = req.params;
+	let { libros } = autores.find(autor => autor.id === parseInt(id));
+	let libro = libros.find(libro => libro.id === parseInt(idLibro));
 	res.send(libro);
+});
+
+server.put('/autores/:id/libros/:idLibro', existeAutorID, existeLibroID, (req, res) => {
+	let { id, idLibro } = req.params;
+
+	const autor = autores.find(autor => autor.id === parseInt(id));
+	let indexAutor = autores.findIndex(autor => autor.id === parseInt(id));
+	let indexLibro = autor.libros.findIndex(libro => libro.id === parseInt(idLibro));
+
+	const nuevoLibro = req.body;
+	nuevoLibro.id = autor.libros[indexLibro].id;
+
+	autor.libros.splice(indexLibro, 1, nuevoLibro);
+	autores.splice(indexAutor, 1, autor);
+
+	res.send(nuevoLibro);
+});
+
+server.delete('/autores/:id/libros/:idLibro', existeAutorID, existeLibroID, (req, res) => {
+	let { id, idLibro } = req.params;
+	const autor = autores.find(autor => autor.id === parseInt(id));
+	let indexAutor = autores.findIndex(autor => autor.id === parseInt(id));
+	let indexLibro = autor.libros.findIndex(libro => libro.id === parseInt(idLibro));
+	autor.libros.splice(indexLibro, 1);
+	autores.splice(indexAutor, 1, autor);
+	res.sendStatus(204);
 });
